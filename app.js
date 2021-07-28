faa69dca999cc96e3602b05b80b9ddfe9fcfeef6
Abdul Latif Mubasir <bj.angel119@gmail.com>
2021-07-28 21:58:22 +0700

    question 2

--- app.js
+++ app.js
@@ -24,3 +24,50 @@
 }
 
 console.log(countNumber([10,10,10,10,10])); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 2. Reverse All Array
+/* 
+    Proses :
+    input [123, 456]
+    123 dibalik menjadi 321
+    456 dibalik menjadi 654
+    654 akan berada di index 0 array dan 321 akan berada di index 1 array
+    return [654, 321]
+
+    [123, 456] -> return [654, 321]
+    [332, 432, 661] -> return [166, 234, 233]
+    [456789, 332] -> return [233, 987654]
+    [] -> return []
+*/
+
+/* const reverseArray = (arr) => {
+    let result = [];
+    let ri = 0;
+    for(let i = arr.length -1; i >= 0 ; i--) {
+        let index = arr[i].toString();
+        let reverseIndex = '';
+        for(let j = index.length -1; j >= 0; j--) reverseIndex += index[j];
+        result[ri] = Number(reverseIndex);
+        ri++;
+    }
+
+    return result;
+} 
+
+console.log(reverseArray([456789, 332])); */
